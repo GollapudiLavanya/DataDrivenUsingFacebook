e4bc00a0b1639b2e19032a3bc8d6bbdf1dcebbe7
GollapuydiLavanya <lavanyagollapudi5166@gmail.com>
2021-09-13 16:21:41 +0530

    [LAVANYA]Data Driven Using PageFactory

---
+++ DataDrivenFB/ReadFromExcel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DataDrivenFramework;
+using System.Diagnostics;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace DataDrivenFB
+{
+
+    public class ReadFromExcel : Base.BaseClass
+    {
+        [Test]
+        public void ReadingDataFromExcelTestMethod()
+        {
+            ExcelOperations.PopulateInCollection(@"C:\Users\lavanya.g\Downloads\TestData.xlsx");
+            driver.FindElement(By.Name("email")).SendKeys(ExcelOperations.ReadData(1, "Username"));
+            System.Threading.Thread.Sleep(2000);
+            driver.FindElement(By.Name("pass")).SendKeys(ExcelOperations.ReadData(1, "Password"));
+            System.Threading.Thread.Sleep(2000);
+            driver.FindElement(By.Name("login")).Click();
+            System.Threading.Thread.Sleep(8000);
+
+        }
+    }
+}
